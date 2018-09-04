0ba847229b5a7d9e7a35a5882a0622748fd8c500
wannabit-jayb <jayb@wannabit.io>
2018-09-04 11:45:46 +0900

    Router 정리 (ng generate module app-routing --flat --module=app)

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -4,13 +4,16 @@ import { RouterModule, Routes } from '@angular/router';
 import { LoginComponent } from './components/login/login.component';
 import { HomeComponent } from './components/home/home.component';
 
-const APP_ROUTES: Routes = [
-  { path: '', component: LoginComponent },
+const routes: Routes = [
+  // pathMatch is used to specify the matching strategy full or prefix. full means that the whole URL's path needs to match by the matching algorithm. prefix means the first route where path matches the start of the URL will be chosen. In the case of empty path if we don't set the full matching strategy then we won't get the desired behavior as any path starts with an empty path.
+  { path: '', redirectTo: 'login', pathMatch: 'full' }, 
+  { path: 'login', component: LoginComponent },
+  { path: 'home', component: HomeComponent },
 ];
 
 @NgModule({
   imports: [
-    RouterModule.forRoot(APP_ROUTES)
+    RouterModule.forRoot(routes)
   ],
   exports: [
     RouterModule
@@ -20,18 +23,3 @@ const APP_ROUTES: Routes = [
 export class AppRoutingModule { 
 
 }
-
-
-// const APP_ROUTES: Routes = [
-//   { path: '', component: LoginComponent },
-//   { path: 'login', component: LoginComponent },
-//   { path: 'notAllowed', component: NotAllowedComponent },
-//   { path: 'accounts', component: AccountsComponent, canActivate: [AuthGuard] },
-//   { path: 'eos-tokens', component: EosTokenComponent, canActivate: [AuthGuard] },
-//   { path: 'erc20-tokens', component: Erc20TokenComponent, canActivate: [AuthGuard] },
-//   { path: 'qrc20-tokens', component: Qrc20TokenComponent, canActivate: [AuthGuard] },
-//   { path: 'versions', component: AppVersionComponent, canActivate: [AuthGuard] }
-
-// ];
-
-// export const RoutingModule = RouterModule.forRoot(APP_ROUTES);
