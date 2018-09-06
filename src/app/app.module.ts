c5d30a9beeff599645b411495863d4156e0fab4d
JayB <jaybkim2@gmail.com>
2018-09-06 23:26:02 +0900

    Minor Fix at Home

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,6 +1,11 @@
 // Modules
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { FormsModule } from '@angular/forms';
+import { HttpClientModule } from '@angular/common/http';
+import { ReactiveFormsModule } from "@angular/forms";
+
+// Routing
 import { AppRoutingModule } from './app-routing.module';
 
 // Components
@@ -8,12 +13,12 @@ import { AppComponent } from './app.component';
 import { FooterComponent } from './components/footer/footer.component';
 import { LoginComponent } from './components/login/login.component';
 import { HomeComponent } from './components/home/home.component';
-
-// Services
-import { GlobalService } from './services/global.service';
 import { SideBarComponent } from './components/side-bar/side-bar.component';
 import { UserComponent } from './components/user/user.component';
 import { ContactComponent } from './components/contact/contact.component';
+
+// Services
+import { GlobalService } from './services/global.service';
 
 @NgModule({
   declarations: [
@@ -27,7 +32,10 @@ import { ContactComponent } from './components/contact/contact.component';
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    FormsModule,
+    HttpClientModule,
+    ReactiveFormsModule
   ],
   providers: [
     GlobalService
--- src/app/components/login/login.component.ts
+++ src/app/components/login/login.component.ts
@@ -1,4 +1,11 @@
 import { Component, OnInit } from '@angular/core';
+import { FormGroup, FormBuilder, Validators } from '@angular/forms';
+import { HttpClient, HttpHeaders } from '@angular/common/http';
+import { Router } from '@angular/router';
+
+import { GlobalService } from './../../services/global.service';
+
+import * as CryptoJS from 'crypto-js';
 
 @Component({
   selector: 'app-login',
@@ -7,9 +14,54 @@ import { Component, OnInit } from '@angular/core';
 })
 export class LoginComponent implements OnInit {
 
-  constructor() { }
+  loginForm: FormGroup;
+  
+  isLogin = false;
+
+  // regular expression
+  emailPattern = /^[0-9a-zA-Z]([-_\.]?[0-9a-zA-Z])*@[0-9a-zA-Z]([-_\.]?[0-9a-zA-Z])*\.[a-zA-Z]{2,3}$/i;
+  passwordPattern = /(?=.*\d)(?=.*[a-z])(?=.*[!,@,#,$,%,^,&,*,?,_,~]).{8,20}/;
+
+  constructor(
+    private formBuilder: FormBuilder,
+    private global: GlobalService,
+    private http: HttpClient,
+  ) {
+    this.loginForm = this.formBuilder.group({
+      'email': ['', Validators.compose([Validators.required, Validators.pattern(this.emailPattern)])],
+      'password': ['', Validators.compose([Validators.required, Validators.pattern(this.passwordPattern), Validators.minLength(8)])],
+      'isInvalid': [false]
+    });
+  }
 
   ngOnInit() {
+
   }
+
+  // convenience getter for easy access to form fields
+
+
+  login(value) {
+    console.log(value.email)
+    console.log(value.password)
+  }
+
+  // login() {
+  //   this.isLogin = true;
+
+  //   // stop here if form is invalid
+  //   if (this.loginForm.invalid) {
+  //     return;
+  //   }
+
+  //   const formModel = this.loginForm.value;
+  //   const formData = new FormData();
+  //   formData.append('email', formModel.email);
+  //   formData.append('password', formModel.password);
+
+  //   console.log(formModel.email)
+  //   console.log(formModel.password)
+
+  // }
 
 }
